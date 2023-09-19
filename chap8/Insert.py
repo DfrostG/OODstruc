e0968190c4af2894d1b7496c10c4118330980b3b
Paramate Jiembanjong <paramate.2547@gmail.com>
2023-09-19 15:26:05 +0700

    avl and bst class

---
+++ chap8/Insert.py
@@ -0,0 +1,21 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+        self.height = 1
+    
+    def __str__(self):
+        return str(self.data)
+
+class AVL:
+
+    ?????
+
+    def printTree(self, node, level = 0):
+        if node != None:
+            self.printTree(node.right, level + 1)
+            print('     ' * level, node)
+            self.printTree(node.left, level + 1)
+
+    ?????
