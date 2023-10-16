a1657f4b1e414a5094a6f679cfa17afb72a078c8
Paramate Jiembanjong <paramate.2547@gmail.com>
2023-10-16 23:05:39 +0700

    16/10

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
-#test
+size = int(input("Enter size : "))
+# for i in range(inp):
+#     print("*", end="")
+# print("*")
+# print("_" * (int(inp) - 2))
+# print("*")
+
+# size = 5  # You can change this to the desired size
+
+for i in range(size):
+    # Loop through columns
+    for j in range(size):
+        # Check if it's an edge or the center
+        if i == 0 or i == size - 1 or j == 0 or j == size - 1:
+            print('*', end='')
+        else:
+            print(' ', end='')
+    # Move to the next line after each row
+    print()
